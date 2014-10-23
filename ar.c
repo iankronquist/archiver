cd82bcd34a16bd9147ac99ff6904a8260891476b
Ian Kronquist <iankronquist@teleport.com>
2014-10-22 20:45:33 -0700

    Change fprintf to fwrite and add debugging stuff

--- ar.c
+++ ar.c
@@ -10,19 +10,25 @@
 
 #include "oscar.h"
 FILE* create_new_archive(char* filename);
-int get_file_meta_data(char* filename, struct oscar_hdr* md);
+int get_file_meta_data(struct oscar_hdr* md, FILE *get_file, char* file_name);
 int write_file_meta_data(struct oscar_hdr* md, FILE *file_out);
 int read_file_meta_data(struct oscar_hdr *md, FILE *file_in);
 int twiddle_meta_data(struct oscar_hdr *md, FILE *file_in);
-int append_file(FILE *in_file, FILE *archive);
+int append_file(FILE *in_file, char* in_file_name, FILE *archive);
 int read_and_write_file(FILE* file_in, FILE *file_out);
 
 int main() {
+    puts("0");
     FILE *new_archive = create_new_archive("test.ar");
+    puts("1");
     FILE *read_file_1 = fopen("1-s.txt", "r");
+    puts("2");
     FILE *read_file_2 = fopen("2-s.txt", "r");
-    append_file(read_file_1, new_archive);
-    append_file(read_file_2, new_archive);
+    puts("3");
+    append_file(read_file_1, "1-s.txt", new_archive);
+    puts("4");
+    append_file(read_file_2, "2-s.txt", new_archive);
+    puts("5");
     fclose(new_archive);
     fclose(read_file_1);
     fclose(read_file_2);
@@ -38,21 +44,16 @@ FILE* create_new_archive(char* filename) {
     return out_file;
 }
 
-int get_file_meta_data(char* filename, struct oscar_hdr* md) {
+int get_file_meta_data(struct oscar_hdr* md, FILE *get_file, char* file_name) {
     struct stat statdat;
-    FILE *get_file;
-    // Check that we can read the file and it exists
-    /*if (access(filename, F_OK | R_OK) == -1) {
-        assert(0);
-    }*/
     // Open the file and stat it
-    get_file = fopen(filename, "w");
-    /*if (fstat((int)get_file, &statdat) != 0) {
-        assert(0);
-    }*/
+    if (fstat((int)get_file, &statdat) != 0) {
+        //assert(0);
+        puts("err!");
+    }
     fclose(get_file);
-    snprintf(&md->oscar_name, OSCAR_MAX_FILE_NAME_LEN, "%s", filename);
-    snprintf(&md->oscar_name_len, 2, "%lu", strlen(filename));
+    snprintf(&md->oscar_name, OSCAR_MAX_FILE_NAME_LEN, "%s", file_name);
+    snprintf(&md->oscar_name_len, 2, "%lu", strlen(file_name));
     snprintf(&md->oscar_cdate, OSCAR_DATE_SIZE, "%ld", statdat.st_birthtime);
     snprintf(&md->oscar_adate, OSCAR_DATE_SIZE, "%ld", statdat.st_atime);
     snprintf(&md->oscar_mdate, OSCAR_DATE_SIZE, "%ld", statdat.st_mtime);
@@ -67,20 +68,18 @@ int get_file_meta_data(char* filename, struct oscar_hdr* md) {
 }
 
 int write_file_meta_data(struct oscar_hdr* md, FILE *file_out) {
-    fprintf(file_out, "%s", md->oscar_name);
-    fprintf(file_out, "%s", md->oscar_name_len);
-    fprintf(file_out, "%s", md->oscar_cdate);
-    fprintf(file_out, "%s", md->oscar_adate);
-    fprintf(file_out, "%s", md->oscar_mdate);
-    fprintf(file_out, "%s", md->oscar_uid);
-    fprintf(file_out, "%s", md->oscar_gid);
-    fprintf(file_out, "%s", md->oscar_mode);
-    fprintf(file_out, "%s", md->oscar_size);
-    fprintf(file_out, "%c", md->oscar_deleted);
-    fprintf(file_out, "%s", md->oscar_sha1);
-    fprintf(file_out, "%s", md->oscar_hdr_end);
-
-    memset(&md->oscar_sha1, 0, OSCAR_SHA_DIGEST_LEN);
+    fwrite(&md->oscar_name, OSCAR_MAX_FILE_NAME_LEN, 1, file_out);
+    fwrite(&md->oscar_name_len, 2, 1, file_out);
+    fwrite(&md->oscar_cdate, OSCAR_DATE_SIZE, 1, file_out);
+    fwrite(&md->oscar_adate, OSCAR_DATE_SIZE, 1, file_out);
+    fwrite(&md->oscar_mdate, OSCAR_DATE_SIZE, 1, file_out);
+    fwrite(&md->oscar_uid, OSCAR_UGID_SIZE, 1, file_out);
+    fwrite(&md->oscar_gid, OSCAR_UGID_SIZE, 1, file_out);
+    fwrite(&md->oscar_mode, OSCAR_MODE_SIZE, 1, file_out);
+    fwrite(&md->oscar_size, OSCAR_MAX_MEMBER_FILE_SIZE, 1, file_out);
+    fwrite(&md->oscar_deleted, 1, 1, file_out);
+    fwrite(&md->oscar_sha1, OSCAR_SHA_DIGEST_LEN, 1, file_out);
+    fwrite(&md->oscar_hdr_end, OSCAR_HDR_END_LEN, 1, file_out);
     return 0;
 }
 
@@ -122,10 +121,10 @@ int twiddle_meta_data(struct oscar_hdr *md, FILE *file_in) {
     return 0;
 }
 
-int append_file(FILE *in_file, FILE *archive) {
+int append_file(FILE *in_file, char* in_file_name, FILE *archive) {
     struct oscar_hdr md;
     fseek(archive, 0, SEEK_END);
-    get_file_meta_data(&md, in_file);
+    get_file_meta_data(&md, in_file, in_file_name);
     write_file_meta_data(&md, archive);
     read_and_write_file(in_file, archive);
     return 0;    
